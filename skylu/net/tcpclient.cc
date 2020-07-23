9f536e63d1698493cc2b99f3d8ea78efcc21f48f
jimlu <jimlu@tencent.com>
2020-07-23 11:11:37 +0800

    增加客户端类

---
+++ skylu/net/tcpclient.cc
@@ -0,0 +1,113 @@
+//
+// Created by jimlu on 2020/7/22.
+//
+
+#include "tcpclient.h"
+
+namespace skylu{
+
+TcpClient::TcpClient(EventLoop *loop, const Address::ptr &server,
+                     const std::string &name)
+:m_loop(loop)
+    ,m_connector(new Connector(loop,server))
+,m_name(name)
+,m_connection_callback(nullptr)
+,m_message_callback(nullptr)
+,m_writeCompleteCallback(nullptr)
+,m_isRetry(false)
+      ,m_enableConnect(false)
+,m_ConnectId(1)
+{
+
+  m_connector->setNewConnectionCallback(
+                  std::bind(&TcpClient::newConnection,this,std::placeholders::_1)
+                  );
+
+}
+TcpClient::~TcpClient() {
+  TcpConnection::ptr conne = nullptr;
+  bool unique = false;
+  {
+    Mutex::Lock lock(m_mutex);
+    unique = m_connection.unique();  // 如果其他的函数还有保留这个副本就会失败
+    conne = m_connection;
+    //写时复制
+  }
+  if(conne){
+    assert(m_loop == conne->getLoop());
+    TcpConnection::CloseCallback  cb = [this](const TcpConnection::ptr &conne){
+
+      m_loop->queueInLoop(std::bind(&TcpConnection::connectDestroyed,conne));
+
+    };
+    m_loop->runInLoop(std::bind(&TcpConnection::setCloseCallback,conne,cb));
+    if(unique){
+      conne->shutdown();
+    }
+
+
+  }else{
+    // 已经remove connection
+    m_connector->stop();
+  }
+
+
+}
+void TcpClient::connect() {
+  m_enableConnect = true;
+  m_connector->start();
+
+}
+void TcpClient::disconnect() {
+  m_enableConnect = false;
+
+  {
+    Mutex::Lock lock(m_mutex);
+    if(m_connection){
+      m_connection->shutdown();
+    }
+  }
+
+}
+void TcpClient::stop() {
+  m_enableConnect = false;
+  m_connector->stop();
+
+}
+void TcpClient::newConnection(const Socket::ptr & sock) {
+  m_loop->assertInLoopThread();
+  char buf[32];
+  sprintf(buf,":%s#%d",sock->getRemoteAddress()->toString().c_str(),m_ConnectId);
+  ++m_ConnectId;
+  TcpConnection::ptr conne(new TcpConnection(m_loop
+                                             ,sock
+                                             ,m_name+buf));
+
+  conne->setConnectionCallback(m_connection_callback);
+  conne->setMessageCallback(m_message_callback);
+  conne->setWriteCompleteCallback(m_writeCompleteCallback);
+  conne->setCloseCallback(std::bind(&TcpClient::removeConnection,this,std::placeholders::_1));
+  {
+    Mutex::Lock lock(m_mutex);
+    m_connection = conne;
+  }
+  conne->connectEstablished();
+
+}
+void TcpClient::removeConnection(const TcpConnection::ptr &conne) {
+  m_loop->assertInLoopThread();
+  assert(m_loop == conne->getLoop());
+  {
+    Mutex::Lock lock(m_mutex);
+    assert(m_connection == conne);
+    m_connection.reset();
+  }
+
+  m_loop->queueInLoop(std::bind(&TcpConnection::connectDestroyed,conne));
+  if(m_isRetry && m_enableConnect){
+    SKYLU_LOG_FMT_DEBUG(G_LOGGER,"TcpClient::removeConnection[%s]|restart to host %s"
+                        ,m_name.c_str(),m_connector->getServerAddress()->toString().c_str());
+    m_connector->restart();
+  }
+}
+}
---
+++ skylu/net/tcpclient.h
@@ -0,0 +1,59 @@
+//
+// Created by jimlu on 2020/7/22.
+//
+
+#ifndef DIRSERVER_TCPCLIENT_H
+#define DIRSERVER_TCPCLIENT_H
+
+#include "eventloop.h"
+#include "connector.h"
+#include "tcpconnection.h"
+
+namespace skylu {
+
+class TcpClient : Nocopyable {
+public:
+  TcpClient(EventLoop* loop,
+            const Address::ptr &server,
+            const  std::string & name);
+  ~TcpClient();
+
+  void connect();
+  void disconnect();
+  void stop();
+  const TcpConnection::ptr& getConnection() const {return m_connection;}
+
+  bool isRetry()const {return m_isRetry;}
+  bool enableConnect() const {return m_enableConnect;}
+  void enableRetry(){m_isRetry = true;}
+
+  const std::string & getName()const {return m_name;}
+
+  void setConnectionCallback(const TcpConnection::ConnectionCallback& cb){m_connection_callback = cb;}
+  void setMessageCallback(const TcpConnection::MessageCallback & cb){m_message_callback = cb;}
+  void setWriteCompleteCallback(const TcpConnection::WriteCompleteCallback &cb){m_writeCompleteCallback = cb;}
+
+private:
+  void newConnection(const Socket::ptr & sock);
+  void removeConnection(const TcpConnection::ptr & conne);
+
+
+
+private:
+  EventLoop * m_loop;
+  Connector::ptr m_connector;
+  const std::string m_name;
+  TcpConnection::ConnectionCallback  m_connection_callback;
+  TcpConnection::MessageCallback  m_message_callback;
+  TcpConnection::WriteCompleteCallback  m_writeCompleteCallback;
+  Mutex m_mutex; // m_connection和m_connector 可能跨线程 需要加锁
+  bool m_isRetry;
+  bool m_enableConnect;
+  int m_ConnectId;  // 连接 ID （用来标识连接次数 ）
+  TcpConnection::ptr m_connection;
+
+
+};
+
+}
+#endif // DIRSERVER_TCPCLIENT_H
